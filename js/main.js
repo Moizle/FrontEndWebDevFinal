9bd7bb9d6ae778222aac272868c92a6a9e02e8c5
Moiz <m_zhara@mail.fhsu.edu>
2020-12-06 21:09:26 -0500

    fixed enter button to search

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ const cityName = document.getElementById("cityEntry").value;
 document.getElementById("cityEntry").addEventListener("keyup", (event) =>
 {
     event.preventDefault();
-    if (event.keyCode === 'Enter')
+    if (event.key === 'Enter')
     {
         input.click();
     }
